0e5e13aa8d7a0c6f101a414d4447283bd51c3eb3
zoltan313 <danielsyahreza@gmail.com>
2021-05-20 19:21:07 +0800

    fix semantic structure

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -23,16 +23,18 @@ let datarest='';
 data.restaurants.forEach((restaurants) => {
     datarest +=`
         <div class="card">
-             <article class="restaurant-item">
-                 <img class="restaurant-thumbnail" src="${restaurants.pictureId}" alt="${restaurants.name}">
-                 <div class="restaurant-item-content">
-                     <h1 class="restaurant-name">${restaurants.name}</h1>
-                     <p class="restaurant-city"><strong>${restaurants.city}</strong></p>
-                     <p class="restaurant-description">${restaurants.description.substring(0,170)}</p>
-                     <p class="restaurant-rating"><strong> Rating: </strong> ${restaurants.rating}</p>
-                     <button class="find-more" type="button" aria-label="see more info">Find More</button>
-                 </div>
-             </article>
+            <section>
+                <article class="restaurant-item">
+                    <img class="restaurant-thumbnail" src="${restaurants.pictureId}" alt="${restaurants.name}">
+                    <div class="restaurant-item-content">
+                        <h1 class="restaurant-name">${restaurants.name}</h1>
+                        <p class="restaurant-city"><strong>${restaurants.city}</strong></p>
+                        <p class="restaurant-description">${restaurants.description.substring(0,170)}</p>
+                        <p class="restaurant-rating"><strong> Rating: </strong> ${restaurants.rating}</p>
+                        <button class="find-more" type="button" aria-label="see more info">Find More</button>
+                    </div>
+                </article>
+            </section>
         </div>
     `;
 });
