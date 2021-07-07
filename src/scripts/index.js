49618eb8bd6c9941c7aff1cc5357c4926e37716d
zoltan313 <danielsyahreza@gmail.com>
2021-07-07 12:33:08 +0800

    add service worker

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,46 +1,20 @@
 import 'regenerator-runtime'; /* for async await transpile */
-import '../assets/main.css';
-import data from '../DATA.json';
+import '../styles/main.css';
+import App from './views/app';
+import swRegister from './utils/sw-register';
 
-console.log(data);
-
-const hamburgerButtonElement = document.querySelector('#hamburger');
-const mainElement = document.querySelector('main');
-const drawerElement = document.querySelector('#drawer');
-
-
-hamburgerButtonElement.addEventListener('click', (event) => {
-  drawerElement.classList.toggle('open');
-  event.stopPropagation();
+const app = new App({
+  button: document.querySelector('#hamburger'),
+  drawer: document.querySelector('#drawer'),
+  content: document.querySelector('#main-content'),
 });
 
-mainElement.addEventListener('click', function() {
-  drawerElement.classList.remove('open');
+window.addEventListener('hashchange', () => {
+  app.renderPage();
 });
 
-let datarest='';
-
-data.restaurants.forEach((restaurants) => {
-  datarest +=`
-        <div class="card">
-            <section>
-                <article class="restaurant-item">
-                    <img class="restaurant-thumbnail" src="${restaurants.pictureId}" alt="${restaurants.name}">
-                    <div class="restaurant-item-content">
-                        <h1 class="restaurant-name">${restaurants.name}</h1>
-                        <p class="restaurant-city"><strong>${restaurants.city}</strong></p>
-                        <p class="restaurant-description">${restaurants.description.substring(0, 170)}</p>
-                        <p class="restaurant-rating"><strong> Rating: </strong> ${restaurants.rating}</p>
-                        <button class="find-more" type="button" aria-label="see more info">Find More</button>
-                    </div>
-                </article>
-            </section>
-        </div>
-    `;
+window.addEventListener('load', () => {
+  app.renderPage();
+  swRegister();
 });
-
-document.getElementById('lists-container').innerHTML = datarest;
-
-console.log('Hello Coders! :)');
-
 
